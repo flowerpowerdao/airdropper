6bed13610f75e5b8a5433023d23bf3cb8fa77184
Moritz <fullermoritz@gmail.com>
2022-07-26 15:17:51 +0200

    ✨ WL from canister reigstry dump

--- get_addresses/main.py
+++ get_addresses/main.py
@@ -1,66 +1,37 @@
-import random
-from ic.canister import Canister
-from ic.client import Client
-from ic.identity import Identity
-from ic.agent import Agent
-import sys
-
-iden = Identity()
-client = Client()
-agent = Agent(iden, client)
+import json
 
 
-def get_registry(canister: Canister):
-    return canister.getRegistry()   # type: ignore
+def get_registry():
+    with open('pk6rk-6aaaa-aaaae-qaazq-cai.json', 'r') as f:
+        return json.load(f)
 
 
 def extract_addresses(registry):
     addresses = []
-    for entry in registry[0]:
-        # # filter for addressses that should be removed here
-        # if entry[1] == '04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96':
-        #     continue
+    for entry in registry:
+        # filter for addressses that should be removed here
+        if entry[1] == '04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96':
+            continue
         addresses.append(entry[1])
     return addresses
 
 
-def dump(addresses, random_indices, total_size):
+def dump(addresses):
     with open('addresses.txt', 'w') as f:
         for index, address in enumerate(addresses):
-            f.write(f'(\"{address}\", {random_indices[index]}),')
-
-    difference = list(
-        set(random_indices).symmetric_difference(range(total_size)))
-
-    with open('for_sale.txt', 'w') as f:
-        for index in random.sample(difference, len(difference)):
-            f.write(f'{index},')
+            f.write(f'\"{address}\",')
 
 
 def main():
-    # read governance candid from file
-    ext_did = open("production.did").read()
 
     # holds concatenation of all addresses
     all_addresses = []
 
-    # get total collection size
-    total_size = int(sys.argv[1])
-
-    # loop over canister
-    for canister_id in sys.argv[2:]:
-        # create a canister instance
-        canister = Canister(
-            agent=agent, canister_id=canister_id, candid=ext_did)
-        # get registry
-        registry = get_registry(canister)
-        # extract the addresses
-        addresses = extract_addresses(registry)
-        all_addresses += addresses
-
-    # create pool of random numbers
-    random_indices = random.sample(
-        range(total_size), len(all_addresses))
+    registry = get_registry()
+    # extract the addresses
+    addresses = extract_addresses(registry)
+    all_addresses += addresses
+    print(len(all_addresses))
 
     # dump addresses with random indices to file
-    dump(all_addresses, random_indices, total_size)
+    dump(all_addresses)
