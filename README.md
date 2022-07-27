32a55960fcd2b1b92f89bb3a338be2551e8d4b5c
Moritz <fullermoritz@gmail.com>
2022-07-27 12:01:48 +0200

    ✨ removed filter

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # usage
 
 -   run `poetry install` from the current directory
--   run `poetry run airdrop <total_collection_size> <canister_id>*`
+-   run `poetry run airdrop`
     > :warning: **If you want to manually add people to the list**: Append their addresses to `all_addresses`! One airdrop per address. This means if someone gets 4 NFTs, append their address to `all_addresses` 4 times.
--- get_addresses/main.py
+++ get_addresses/main.py
@@ -9,9 +9,10 @@ def get_registry():
 def extract_addresses(registry):
     addresses = []
     for entry in registry:
-        # filter for addressses that should be removed here
-        if entry[1] == '04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96':
-            continue
+        # # filter for addressses that should be removed here
+        # # filter toniq earn address
+        # if entry[1] == '04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96':
+        #     continue
         addresses.append(entry[1])
     return addresses
 
