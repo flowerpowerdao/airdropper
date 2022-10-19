2da12b5770333c9028be691fb0a0b5536a3a1e1b
Moritz <fullermoritz@gmail.com>
2022-10-19 09:47:04 +0200

    ✨ formatted with black

--- get_addresses/main.py
+++ get_addresses/main.py
@@ -11,30 +11,32 @@ agent = Agent(iden, client)
 
 
 def get_registry(canister: Canister):
-    return canister.getRegistry()   # type: ignore
+    return canister.getRegistry()  # type: ignore
 
 
 def extract_addresses(registry):
     addresses = []
     for entry in registry[0]:
-        # # filter for addressses that should be removed here
-        # if entry[1] == '04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96':
-        #     continue
+        # filter for addressses that should be removed here
+        if (
+            entry[1]
+            == "04986920ad70b5e959851d8ac323a4cf3c0e164311cdd2b7da815c9b9553ba96"
+        ):  # toniq earn address
+            continue
         addresses.append(entry[1])
     return addresses
 
 
 def dump(addresses, random_indices, total_size):
-    with open('addresses.txt', 'w') as f:
+    with open("addresses.txt", "w") as f:
         for index, address in enumerate(addresses):
-            f.write(f'(\"{address}\", {random_indices[index]}),')
+            f.write(f'("{address}", {random_indices[index]}),')
 
-    difference = list(
-        set(random_indices).symmetric_difference(range(total_size)))
+    difference = list(set(random_indices).symmetric_difference(range(total_size)))
 
-    with open('for_sale.txt', 'w') as f:
+    with open("for_sale.txt", "w") as f:
         for index in random.sample(difference, len(difference)):
-            f.write(f'{index},')
+            f.write(f"{index},")
 
 
 def main():
@@ -50,8 +52,7 @@ def main():
     # loop over canister
     for canister_id in sys.argv[2:]:
         # create a canister instance
-        canister = Canister(
-            agent=agent, canister_id=canister_id, candid=ext_did)
+        canister = Canister(agent=agent, canister_id=canister_id, candid=ext_did)
         # get registry
         registry = get_registry(canister)
         # extract the addresses
@@ -59,8 +60,7 @@ def main():
         all_addresses += addresses
 
     # create pool of random numbers
-    random_indices = random.sample(
-        range(total_size), len(all_addresses))
+    random_indices = random.sample(range(total_size), len(all_addresses))
 
     # dump addresses with random indices to file
     dump(all_addresses, random_indices, total_size)
